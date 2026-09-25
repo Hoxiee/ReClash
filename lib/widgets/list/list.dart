b98a8df1d21b5ba5808fb400ccfe5b5be5f7f51b
Hoxiee <hoxiee.dev@gmail.com>
2026-09-25 23:46:08 +0300

    refactor(widgets): remove dead UI helpers with no call sites
    - drop TooltipTextV2, SettingsBlock, generateInfoSection, generateListView

--- lib/widgets/list/list.dart
+++ lib/widgets/list/list.dart
@@ -641,26 +641,3 @@ Widget generateSectionV3({
     ],
   );
 }
-
-List<Widget> generateInfoSection({
-  required Info info,
-  required Iterable<Widget> items,
-  List<Widget>? actions,
-  bool separated = true,
-}) {
-  final genItems = separated
-      ? items.separated(const Divider(height: 0))
-      : items;
-  return [
-    if (items.isNotEmpty) InfoHeader(info: info, actions: actions),
-    ...genItems,
-  ];
-}
-
-Widget generateListView(List<Widget> items) {
-  return ListView.builder(
-    itemCount: items.length,
-    itemBuilder: (_, index) => items[index],
-    padding: const EdgeInsets.only(bottom: 16),
-  );
-}
